3a03f79685d706a7529f001b965ff3e362380593
Intellix <141814641+IntellsGamer@users.noreply.github.com>
2026-08-30 12:36:20 +0330

    Update NoFallCheck.java

--- src/main/java/dev/idebugger/nyx/checks/movement/NoFallCheck.java
+++ src/main/java/dev/idebugger/nyx/checks/movement/NoFallCheck.java
@@ -155,7 +155,7 @@ public class NoFallCheck extends Check {
         if (onGround && !isNearGround(packetY, player)) {
             data.incrementNofallCorrections();
             // plugin.getLogger().info(String.format("%s NoFall correction #%d SPOOF FD:%.2f",
-                player.getName(), data.getNofallCorrections(), totalAccum));
+            //    player.getName(), data.getNofallCorrections(), totalAccum));
             applyFullFallDamage(player, totalAccum);
         }
     }
